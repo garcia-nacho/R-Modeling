d78708ef983db5e76b00c5015f22bd23fc28aae3
Nacho Garcia <garcia.nacho@gmail.com>
2018-11-17 18:46:45 +0100

    Update app.R

--- ReplicatorSIm/app.R
+++ ReplicatorSIm/app.R
@@ -1,4 +1,4 @@
-#  Numerical simulation for eplicators interacting
+#  Numerical simulation for a population of 
 #  Nacho garcia 2018
 #  garcia.nacho ---AT--- gmail.com
 #  GPL v3
@@ -18,10 +18,10 @@ ui <- fluidPage(
   # Sidebar 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("Cell", "Number of replicators:", 1,60,20),
-      sliderInput("Pert", "Perturbation strength:", 0,100,50),
-      sliderInput("Pos", "Positive interaction:", 0,100,50),
-      sliderInput("Neg", "Negative interaction:", 0,100,50),
+      sliderInput("Cell", "Number of Replicators:", 1,60,20),
+      sliderInput("Pert", "Perturbation Strength:", 0,100,50),
+      sliderInput("Pos", "Positive Interaction:", 0,100,50),
+      sliderInput("Neg", "Negative Interaction:", 0,100,50),
       actionButton("do", "RUN", align = "center"),
       
       h6("By Nacho Garcia 2018", align = "left"),
@@ -33,7 +33,12 @@ ui <- fluidPage(
     # Plot
     mainPanel(
       
-      plotOutput("Plot")
+      tabsetPanel(
+        tabPanel("Frequencies", plotOutput("Plot")),
+        tabPanel("Count", plotOutput("Plot2")),
+        tabPanel("Help", htmlOutput("help"))
+      )
+      
       
     )
   )
@@ -48,7 +53,7 @@ server <- function(input, output) {
     Cells<-input$Cell
     Pert<-(input$Pert+1)/100  #Range (0.1-1)
     R <-rlnorm(Cells,1,Pert)
-    R <- 3+R*-0.1
+    R <- 3+R*-0.01
     tf<-20
     
     FTc<-c(1:tf)
@@ -63,6 +68,7 @@ server <- function(input, output) {
     
     #Interaction 0 at t=0
     FT<-0
+    total<-c(Cells)
     
     #Number of cells at t=0
     C[1,]<-1/Cells
@@ -75,6 +81,7 @@ server <- function(input, output) {
       }
       
       a<-sum(C[t,])
+      total[t]<-a
       for(v in 1:Cells){
         
         C[t,v]<-C[t,v]/a
@@ -93,23 +100,57 @@ server <- function(input, output) {
     C$t<-c(1:tf)
     C<-melt(C, id.vars = "t")
     colnames(C)<-c("Time","variable","Ratio")
+    Abs<-as.data.frame(total)
+    colnames(Abs)<-"Total"
+    Abs$Time<-c(1:nrow(Abs))
     
     output$Plot <- renderPlot( { 
 
       q<-ggplot(C)+
-        geom_line(aes(x=Time, y=Ratio, colour=variable))+
-        theme_minimal()+
-        ylim(0,1)+
-        theme(legend.position="none")
+        geom_line(aes(x=Time, y=Ratio, colour=variable),size=1.5)+
+        theme_minimal(base_size = 20)+
+        #ylim(0,1)+
+        theme(legend.position="none")+
+        ggtitle("Evolution of Ratios")
       
       print(q)
+          })
+    
+    output$Plot2 <- renderPlot( { 
       
+      r<-ggplot(Abs)+
+        geom_line(aes(x=Time, y=log(Total)),size=1.5, colour="green")+
+        theme_minimal(base_size = 20)+
+        #ylim(0,1)+
+        theme(legend.position="none")+
+        ggtitle("Absolute Number")+
+        ylab("log(Count)")
       
-      
+      print(r)
     })
     
+
   })        
-}
+
+  output$help <- renderUI({
+    
+    HTML(paste(
+      "This numerical simulation provides an exploratory framework to study how positive or negative genetic perturbations evolve with time.",
+      "",
+      "<b>Number of Replicators:</b> Number of replicating entities to be simulated ",
+      "",
+      "<b>Perturbation Strength:</b> How much the fitness of the different replicators is affected by the perturbation (positive or negative)",
+      "",    
+      "<b>Positive interaction:</b> Strength of the positive interaction between replicators. This can be understood as replicators producing stimulating chemicals",
+      "",
+      "<b>Negative interaction:</b> Strength of the negative interaction between replicators. This can be understood as replicators producing toxic compounds",
+      sep = "</br>"
+      
+      
+    ))
+    
+  })
+  
+  }
 # Run the application 
 shinyApp(ui = ui, server = server)
-
